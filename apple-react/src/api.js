7889ca113daab2642e6a5db84fe290024ddb9a76
matiwosyabibal <matiwosyabibal@gmail.com>
2025-11-23 11:15:07 +0300

    Update api.js

--- apple-react/src/api.js
+++ apple-react/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend server URL
+  baseURL: "https://sunday-market-web-app.onrender.com", // backend server URL
 });
 
 // Attach auth token and user id to all requests
